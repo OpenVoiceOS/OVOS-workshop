52af7b1a38592943f865a983ffca9198c9a12015
jarbasai <jarbasai@mailfence.com>
2021-11-19 09:33:34 +0000

    feat/converse_intents
    add support for .intent files in converse method
    
    a new decorator can be used to define internal intent handlers

--- ovos_workshop/decorators/__init__.py
+++ ovos_workshop/decorators/__init__.py
@@ -1,10 +1,13 @@
+from functools import wraps
+
 from ovos_utils.log import log_deprecation
+
 from ovos_workshop.decorators.killable import killable_intent, killable_event
 from ovos_workshop.decorators.layers import enables_layer, \
     disables_layer, layer_intent, removes_layer, resets_layers, replaces_layer
 from ovos_workshop.decorators.ocp import ocp_play, ocp_pause, ocp_resume, \
     ocp_search, ocp_previous, ocp_featured_media
-from functools import wraps
+
 
 # TODO: Deprecate unused import retained for backwards-compat.
 from ovos_utils import classproperty
@@ -25,7 +28,9 @@ def adds_context(context: str, words: str = ''):
             ret = func(*args, **kwargs)
             args[0].set_context(context, words)
             return ret
+
         return func_wrapper
+
     return context_add_decorator
 
 
@@ -43,7 +48,9 @@ def removes_context(context: str):
             ret = func(*args, **kwargs)
             args[0].remove_context(context)
             return ret
+
         return func_wrapper
+
     return context_removes_decorator
 
 
@@ -68,6 +75,7 @@ def intent_file_handler(intent_file: str):
     """
     Deprecated decorator for adding a method as an intent file handler.
     """
+
     def real_decorator(func):
         # Store the intent_file inside the function
         # This will be used later to call register_intent_file
@@ -75,6 +83,7 @@ def intent_file_handler(intent_file: str):
             func.intent_files = []
         func.intent_files.append(intent_file)
         return func
+
     log_deprecation(f"Use `@intent_handler({intent_file})`", "0.1.0")
     return real_decorator
 
@@ -118,6 +127,22 @@ def converse_handler(func):
     return func
 
 
+def conversational_intent(intent_file):
+    """Decorator for adding a method as an converse intent handler.
+    NOTE: only padatious intents supported, not adapt
+    """
+
+    def real_decorator(func):
+        # Store the intent_file inside the function
+        # This will be used later to train intents
+        if not hasattr(func, 'converse_intents'):
+            func.converse_intents = []
+        func.converse_intents.append(intent_file)
+        return func
+
+    return real_decorator
+
+
 def fallback_handler(priority: int = 50):
     """
     Decorator for adding a fallback intent handler.
@@ -125,6 +150,7 @@ def fallback_handler(priority: int = 50):
     @param priority: Fallback priority (0-100) with lower values having higher
         priority
     """
+
     def real_decorator(func):
         if not hasattr(func, 'fallback_priority'):
             func.fallback_priority = priority
