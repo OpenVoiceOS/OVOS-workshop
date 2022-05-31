b44d719ef9bf63205afab5967377a8e73cc42d8b
jarbasai <jarbasai@mailfence.com>
2022-05-31 17:58:39 +0100

    fix/do not require mycroft

--- ovos_workshop/skills/__init__.py
+++ ovos_workshop/skills/__init__.py
@@ -1,8 +1,10 @@
-import inspect
+try:
+    from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill,     OVOSFallbackSkill
+    from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
+except ImportError:
+    pass
+    # if mycroft is not available do not export the skill class
+    # this is common in OvosAbstractApp implementations such as OCP
 
-
-from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill, \
-    OVOSFallbackSkill
-from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
 from ovos_workshop.skills.layers import IntentLayers
 
