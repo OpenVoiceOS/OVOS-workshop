2aebc00912d87b9a75d06dc7f0f3a701c658ebaf
Daniel McKnight <daniel@neongecko.com>
2023-01-16 14:27:52 -0800

    Cleanup logging for backwards-compat init

--- ovos_workshop/skills/__init__.py
+++ ovos_workshop/skills/__init__.py
@@ -1,14 +1,19 @@
+import logging
+
 try:
     from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill, OVOSFallbackSkill
     from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
 except ImportError as e:
-    import inspect
-    from pprint import pformat
     from ovos_utils.log import LOG
-    for call in inspect.stack():
-        module = inspect.getmodule(call.frame)
-        name = module.__name__ if module else call.filename
-        LOG.info(f"{name}:{call.lineno}")
+    LOG.warning(e)
+    if LOG.level == logging.DEBUG:
+        # This can really slow down init on a Pi, only init if we're going to log
+        import inspect
+        from pprint import pformat
+        for call in inspect.stack():
+            module = inspect.getmodule(call.frame)
+            name = module.__name__ if module else call.filename
+            LOG.debug(f"{name}:{call.lineno}")
 
     # if mycroft is not available do not export the skill class
     # this is common in OvosAbstractApp implementations such as OCP
