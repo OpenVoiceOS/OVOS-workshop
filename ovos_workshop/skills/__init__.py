9748a14e51e623ce12849a63555bd347dd392ec0
Daniel McKnight <daniel@neongecko.com>
2023-01-16 16:12:57 -0800

    Cleanup import logic

--- ovos_workshop/skills/__init__.py
+++ ovos_workshop/skills/__init__.py
@@ -1,11 +1,3 @@
-try:
-    from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill, OVOSFallbackSkill
-    from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
-except ImportError as e:
-    from ovos_utils.log import LOG
-    LOG.warning(e)
-    # if mycroft is not available do not export the skill class
-    # this is common in OvosAbstractApp implementations such as OCP
-
+from ovos_workshop.skills.ovos import MycroftSkill, OVOSSkill, OVOSFallbackSkill
+from ovos_workshop.skills.idle_display_skill import IdleDisplaySkill
 from ovos_workshop.decorators.layers import IntentLayers
-
