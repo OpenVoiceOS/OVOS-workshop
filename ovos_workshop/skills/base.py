7938ebdadcc341884059068d8e1079e96e10f238
jarbasai <jarbasai@mailfence.com>
2023-02-08 23:53:56 +0000

    renaming

--- ovos_workshop/skills/base.py
+++ ovos_workshop/skills/base.py
@@ -55,7 +55,7 @@ from ovos_workshop.decorators.killable import killable_event, \
 from ovos_workshop.filesystem import FileSystemAccess
 from ovos_workshop.resource_files import ResourceFile, \
     CoreResources, SkillResources, find_resource
-from ovos_utils.network_utils import NetworkRequirements as SkillNetworkRequirements
+from ovos_utils.process_utils import RuntimeRequirements
 
 
 def simple_trace(stack_trace):
@@ -141,14 +141,14 @@ class BaseSkill:
 
     # classproperty not present in mycroft-core
     @classproperty
-    def network_requirements(self):
+    def runtime_requirements(self):
         """ skill developers should override this if they do not require connectivity
 
          some examples:
 
          IOT skill that controls skills via LAN could return:
             scans_on_init = True
-            SkillNetworkRequirements(internet_before_load=False,
+            RuntimeRequirements(internet_before_load=False,
                                      network_before_load=scans_on_init,
                                      requires_internet=False,
                                      requires_network=True,
@@ -157,7 +157,7 @@ class BaseSkill:
 
          online search skill with a local cache:
             has_cache = False
-            SkillNetworkRequirements(internet_before_load=not has_cache,
+            RuntimeRequirements(internet_before_load=not has_cache,
                                      network_before_load=not has_cache,
                                      requires_internet=True,
                                      requires_network=True,
@@ -165,14 +165,14 @@ class BaseSkill:
                                      no_network_fallback=True)
 
          a fully offline skill:
-            SkillNetworkRequirements(internet_before_load=False,
+            RuntimeRequirements(internet_before_load=False,
                                      network_before_load=False,
                                      requires_internet=False,
                                      requires_network=False,
                                      no_internet_fallback=True,
                                      no_network_fallback=True)
         """
-        return SkillNetworkRequirements()
+        return RuntimeRequirements()
 
     # property not present in mycroft-core
     @property
