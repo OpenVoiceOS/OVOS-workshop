c4d967180441b492af71f59d532bbb9ed4170361
Daniel McKnight <daniel@neongecko.com>
2023-07-06 21:37:48 -0700

    Log deprecation for backwards-compat decorators Cleanup, document, and test active and auto_translatable skill modules

---
+++ test/unittests/skills/test_auto_translatable.py
@@ -0,0 +1,40 @@
+import unittest
+
+from ovos_workshop.skills.common_query_skill import CommonQuerySkill
+from ovos_workshop.skills.fallback import FallbackSkill
+from ovos_workshop.skills.base import BaseSkill
+
+
+class TestUniversalSkill(unittest.TestCase):
+    from ovos_workshop.skills.auto_translatable import UniversalSkill
+    test_skill = UniversalSkill()
+
+    def test_00_init(self):
+        self.assertIsInstance(self.test_skill, self.UniversalSkill)
+        self.assertIsInstance(self.test_skill, BaseSkill)
+
+    # TODO: Test other class methods
+
+
+class TestUniversalFallbackSkill(unittest.TestCase):
+    from ovos_workshop.skills.auto_translatable import UniversalFallback
+    test_skill = UniversalFallback()
+
+    def test_00_init(self):
+        self.assertIsInstance(self.test_skill, self.UniversalFallback)
+        self.assertIsInstance(self.test_skill, BaseSkill)
+        self.assertIsInstance(self.test_skill, FallbackSkill)
+
+    # TODO: Test other class methods
+
+
+class TestUniversalCommonQuerySkill(unittest.TestCase):
+    from ovos_workshop.skills.auto_translatable import UniversalCommonQuerySkill
+    test_skill = UniversalCommonQuerySkill()
+
+    def test_00_init(self):
+        self.assertIsInstance(self.test_skill, self.UniversalCommonQuerySkill)
+        self.assertIsInstance(self.test_skill, BaseSkill)
+        self.assertIsInstance(self.test_skill, CommonQuerySkill)
+
+    # TODO: Test other class methods
