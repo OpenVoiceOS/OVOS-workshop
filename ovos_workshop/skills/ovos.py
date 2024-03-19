23feef7b169698ab79fb103f8400b435a856a0ad
miro <jarbasai@mailfence.com>
2024-03-19 17:01:55 +0000

    companion to OpenVoiceOS/ovos-core#325

--- ovos_workshop/skills/ovos.py
+++ ovos_workshop/skills/ovos.py
@@ -850,7 +850,6 @@ class OVOSSkill(metaclass=_OVOSSkillMetaclass):
             self._settings_watchdog = \
                 FileWatcher([self._settings.path],
                             callback=self._handle_settings_file_change,
-                            recursive=True,
                             ignore_creation=True)
 
     def _register_decorated(self):
