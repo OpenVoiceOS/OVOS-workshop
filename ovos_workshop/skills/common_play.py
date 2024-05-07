0bd8a29fb012619740be528fa95eb0cc86a98d7f
Daniel McKnight <daniel@neon.ai>
2024-05-07 16:58:12 -0700

    Add back missing imports

--- ovos_workshop/skills/common_play.py
+++ ovos_workshop/skills/common_play.py
@@ -14,7 +14,7 @@ from ovos_workshop.skills.ovos import OVOSSkill
 # backwards compat imports, do not delete, skills import from here
 from ovos_workshop.decorators.ocp import ocp_play, ocp_next, ocp_pause, ocp_resume, ocp_search, \
     ocp_previous, ocp_featured_media, MediaType, MediaState, MatchConfidence, \
-    ocp_previous, ocp_featured_media
+    PlaybackType, PlaybackMode, PlayerState, LoopState, TrackState
 
 
 def get_non_properties(obj):
